fe02076e1b6241b65f1fa3cf7d5e151fd0d7d400
MelissaCHABANE <113503140+MelissaCHABANE@users.noreply.github.com>
2025-01-31 11:57:34 +0100

    Update siteIshak.js

--- siteIshak.js
+++ siteIshak.js
@@ -1,4 +1,3 @@
-const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yes = document.querySelector(".yes");
@@ -6,18 +5,11 @@ const no = document.querySelector(".no");
 
 yes.addEventListener("click", () => {
     question.innerHTML = "I LOVE YOU TOO! ❤️";
-    gif.src = "https://media.giphy.com/media/jUwpNzg9IcyrK/giphy.gif"; // URL du GIF
-    gif.classList.remove("hidden"); // Afficher l’image
 });
 
-no.style.position = "absolute"; // S'assurer qu'il peut bouger
-
 no.addEventListener("mouseover", () => {
-    const noRect = no.getBoundingClientRect();
-    
-    // Limites pour éviter que le bouton sorte de l’écran
-    const maxX = window.innerWidth - noRect.width;
-    const maxY = window.innerHeight - noRect.height;
+    const maxX = window.innerWidth - no.clientWidth;
+    const maxY = window.innerHeight - no.clientHeight;
 
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
